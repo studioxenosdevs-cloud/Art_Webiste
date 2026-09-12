b6fa55e665445b2cec69491c32532056190b1d4a
studioxenosdevs-cloud <studioxenos.devs@gmail.com>
2026-09-12 15:51:51 +0500

    empty

--- src/components/admin/AdminDashboard.tsx
+++
@@ -1,8 +0,0 @@
-import { Outlet } from 'react-router-dom';
-import { useGalleryStore } from '@/hooks/useGalleryStore';
-
-export default function AdminDashboard() {
-  const store = useGalleryStore();
-
-  return <Outlet context={store} />;
-}
--- src/components/admin/useStore.ts
+++
@@ -1,6 +0,0 @@
-import { useOutletContext } from 'react-router-dom';
-import { useGalleryStore } from '@/hooks/useGalleryStore';
-
-export function useStore() {
-  return useOutletContext<ReturnType<typeof useGalleryStore>>();
-}
